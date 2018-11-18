df430633fcb717c29f5d2b88eea8691805db4c0a
Tom <18158510+Ran-snow@users.noreply.github.com>
2018-11-18 21:58:55 +0800

    Initialization

---
+++ Acme.SimpleTaskApp/3.8.0/src/Acme.SimpleTaskApp.EntityFrameworkCore/EntityFrameworkCore/SimpleTaskAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Abp.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Acme.SimpleTaskApp.EntityFrameworkCore
+{
+    public class SimpleTaskAppDbContext : AbpDbContext
+    {
+        //Add DbSet properties for your entities...
+
+        public SimpleTaskAppDbContext(DbContextOptions<SimpleTaskAppDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
